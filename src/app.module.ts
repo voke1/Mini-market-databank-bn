ab55c4c4601cc4aaa7615cba3f4f3840d9827dcc
voke1 <vokeolomu@yahoo.com>
2020-05-22 10:52:07 +0100

    create module for markets and users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { MarketsController } from './markets/markets.controller';
+import { MarketsService } from './markets/markets.service';
+import { MarketsModule } from './markets/markets.module';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MarketsModule],
+  controllers: [AppController, UserController, MarketsController],
+  providers: [AppService, MarketsService, UserService],
 })
 export class AppModule {}
